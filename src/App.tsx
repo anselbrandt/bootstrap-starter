464585363d80d7e3d1abbbc2490681493ff99aa8
ansel brandt <ansel@anselbrandt.com>
2022-11-03 10:56:37 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react'
+import './App.css'
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Test components here
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
